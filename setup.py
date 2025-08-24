38bb0af4df7de6946762474928c0f7471d11e013
suman jaiswal <jsonusuman351@gmail.com>
2025-08-25 00:41:37 +0530

    second setup commit

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 setup(
-    name='',
+    name='Nextwordpredict',
     version='0.0.1',
     author='suman jaiswal',
     author_email='jsonusuman351@gmail.com',
